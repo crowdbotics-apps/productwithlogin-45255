e05bc5e3b29da64c0518e5abd322edc17ec72cf5
Raj sharma <kevinrinta2018@gmail.com>
2024-01-03 11:31:05 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,15 +1,14 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "productwithlogin-45255",
-  url: "https://productwithlogin-45255.botics.co",
-  api: "https://productwithlogin-45255.botics.co/api/v1",
-  screenOptions: {
-    headerShown: false
-  }
-}
+  "name": "productwithlogin-45255",
+  "url": "https://productwithlogin-45255.botics.co",
+  "api": "https://productwithlogin-45255.botics.co/api/v1",
+  "screenOptions": {
+    "headerShown": false
+  },
+  "initialRoute": "2FA"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
